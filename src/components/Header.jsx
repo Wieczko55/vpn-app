a6f0394c35821b647fcfe3cac715d5c5055fb144
wieczko55 <wieczko55@gmail.com>
2024-03-07 19:37:45 +0100

    sample

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () =>{
                      eraseDelay={400}
                 />
             </h1> 
-            <p className='mt-5 font-semibold text-gray-600 text-xl sm:text-2xl md:text-3xl lg:text-4xl'>avoid the threats waiting for you on the Internet, be less noticeable.</p>
+            <p className='mt-5 font-semibold text-gray-600 text-lg sm:text-2xl md:text-3xl lg:text-4xl'>avoid the threats waiting for you on the Internet, be less noticeable.</p>
             <button className='mt-5 border border-blue-400 rounded-xl p-3 font-semibold text-xl sm:text-2xl md:text-3xl bg-blue-400 shadow-lg shadow-blue-700'>Download</button>
              </div>
         </div>
