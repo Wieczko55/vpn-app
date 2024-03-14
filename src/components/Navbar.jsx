bb3ba67e0588697ed46f7b20315617b7b5227805
wieczko55 <wieczko55@gmail.com>
2024-03-14 18:28:08 +0100

    sample

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,17 +22,17 @@ const Navbar = () => {
             <img src={key} alt="/" className="w-[110px] h-[45px]"/>
            </div>
            <div className="flex items-center sm:*:mx-2 md:*:mx-2 lg:*:mx-3 hover:*:text-blue-500">
-                <p className="hidden md:block">Price</p>
-                <p className="hidden md:block">Functions</p>
-                <p className="hidden md:block">Servers</p>
-                <p className="hidden md:block">Download</p>
-                <p className="hidden lg:block">Blog</p>
-                <p className="hidden lg:block">For companies</p>
+                <a href="#" className="hidden md:block">Price</a>
+                <a href="#" className="hidden md:block">Functions</a>
+                <a href="#" className="hidden md:block">Servers</a>
+                <a href="#" className="hidden md:block">Download</a>
+                <a href="#" className="hidden lg:block">Blog</a>
+                <a href="" className="hidden lg:block">For companies</a>
            </div>
            <div className="flex items-center  sm:ml-0 sm:*:mx-1.3 lg:*:m-3 md:*:mx-3">
                 <button className="border border-blue-500 rounded-xl p-1 hover:bg-blue-500 hover:text-white hover:shadow-lg hover:shadow-blue-500 ">Download</button>
-                <p className="hover:text-blue-500 text-sm sm:text-md ml-2.5">Log in</p>
-                <p className="sm:block hidden hover:text-blue-500 text-sm sm:text-md ml-1.5">Help</p>
+                <a href="#" className="hover:text-blue-500 text-sm sm:text-md ml-2.5">Log in</a>
+                <a href="#" className="sm:block hidden hover:text-blue-500 text-sm sm:text-md ml-1.5">Help</a>
                 <HiOutlineMenu onClick={() => setNav(!nav)} size={30} className="hover:cursor-pointer hover:skew-x-5 mr-2 ml-2.5"/>
            </div>
            {!nav ? <div onClick={()=> setNav(!nav)} className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0 duration-900"></div> : ''}
@@ -41,13 +41,13 @@ const Navbar = () => {
                     <img src={key1} alt="/" />
                     <nav>
                         <ul className='flex flex-col py-4 text-gray-800'>
-                            <li className='flex py-4 text-xl first:pt-2 border-y hover:bg-slate-200 w-full'><TbTruckDelivery size={25} className='mr-4 ml-3'/>Orders</li>
-                            <li className='flex py-4 text-xl border-y hover:bg-slate-200'><MdFavorite size={25} className='mr-4 ml-3'/>Favourites</li>
-                            <li className='flex py-4 text-xl border-y hover:bg-slate-200'><FaWallet size={25} className='mr-4 ml-3'/>Wallet</li>
-                            <li className='flex py-4 text-xl border-y hover:bg-slate-200'><IoMdHelp  size={25} className='mr-4 ml-3'/>Help</li>
-                            <li className='flex py-4 text-xl border-y hover:bg-slate-200'><AiFillTag size={25} className='mr-4 ml-3'/>Promotions</li>
-                            <li className='flex py-4 text-xl border-y hover:bg-slate-200'><BsFillSaveFill size={25} className='mr-4 ml-3'/>Best Ones</li>
-                            <li className='flex py-4 text-xl last:pb-2 border-y hover:bg-slate-200'><FaUserFriends size={25} className='mr-4 ml-3'/>Invite Friends</li>
+                            <li className='cursor-poinnter flex py-4 text-xl first:pt-2 border-y hover:bg-slate-200 w-full'><TbTruckDelivery size={25} className='mr-4 ml-3'/>Orders</li>
+                            <li className='cursor-poinnter flex py-4 text-xl border-y hover:bg-slate-200'><MdFavorite size={25} className='mr-4 ml-3'/>Favourites</li>
+                            <li className='cursor-poinnter flex py-4 text-xl border-y hover:bg-slate-200'><FaWallet size={25} className='mr-4 ml-3'/>Wallet</li>
+                            <li className='cursor-poinnter flex py-4 text-xl border-y hover:bg-slate-200'><IoMdHelp  size={25} className='mr-4 ml-3'/>Help</li>
+                            <li className='cursor-poinnter flex py-4 text-xl border-y hover:bg-slate-200'><AiFillTag size={25} className='mr-4 ml-3'/>Promotions</li>
+                            <li className='cursor-poinnter flex py-4 text-xl border-y hover:bg-slate-200'><BsFillSaveFill size={25} className='mr-4 ml-3'/>Best Ones</li>
+                            <li className='cursor-poinnter flex py-4 text-xl last:pb-2 border-y hover:bg-slate-200'><FaUserFriends size={25} className='mr-4 ml-3'/>Invite Friends</li>
                         </ul>
                     </nav>
                     <div className="absolute sm:relative bottom-0 left-0 w-full">
